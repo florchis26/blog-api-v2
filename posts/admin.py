914c5b6faa3ceef138c7d0e8241192399874eb15
Flor Subillaga <subillagaflor@gmail.com>
2025-02-22 16:29:34 -0400

    post, list and detail

---
+++ posts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted', 'updated')
+    list_filter = ('author', 'date_posted')
+    search_fields = ('title', 'content', 'author__username')
+    ordering = ('-date_posted',)
+
+
+admin.site.register(Post, PostAdmin)
