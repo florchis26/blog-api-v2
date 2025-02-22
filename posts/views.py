e0ddf06f9368511373c9301f54e2aa621f376dd0
Flor Subillaga <subillagaflor@gmail.com>
2025-02-22 18:04:41 -0400

    custom permission

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly)
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
