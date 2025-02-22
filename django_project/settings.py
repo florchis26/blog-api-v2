e0ddabcad450fe026a97d30ab246a617d9e8b3a4
Flor Subillaga <subillagaflor@gmail.com>
2025-02-22 17:21:06 -0400

    cors headers

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posts',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -50,12 +51,23 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+
+]
+
+CORS_TRUSTED_ORGINS = [
+    'http://localhost:3000',
+] 
 
 ROOT_URLCONF = 'django_project.urls'
 
